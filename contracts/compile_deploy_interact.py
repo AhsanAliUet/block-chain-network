616bb71b9e62aa1c0deae6bfdc118b52c24116e5
AhsanAliUet <ahsan.uet.ee.115@gmail.com>
2024-06-12 16:35:43 +0500

    Some minor changes in the compiling script

--- contracts/compile_deploy_interact.py
+++ contracts/compile_deploy_interact.py
@@ -31,6 +31,7 @@ compiled_sol = compile_standard(
 with open(smart_contract_file_path + smart_contract_file + ".json", "w") as file:
     json.dump(compiled_sol, file)
 
+# retrieve bytecode
 bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
     "bytecode"
 ]["object"]
@@ -40,18 +41,20 @@ abi = json.loads(
     compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
 )["output"]["abi"]
 
-# set up connection
+# ===================== Establishing the connection with the network =====================
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:22000"))
 chain_id = 10
-sender_account = "0x30af42e072068e7bff8ddce6d5ee59d4fd2c6694"
+sender_account = "0xa6d3ac044d58c6f4a838c1127034d03fe0720194"
 my_address = Web3.to_checksum_address(sender_account)
 # private_key = os.getenv("PRIVATE_KEY")
-private_key = "0xd9cdee3121681abd782c342938683f51bdf76909d77b67cb8be846230f05be62"
-# initialize contract
+private_key = "0x1f773492dc6c001a5f73cb4adccc5393f84e88f65cdf3055fda23a7d55d4a06f"
+
+# ===================== Instantiate the contract ===================== 
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
 nonce = w3.eth.get_transaction_count(my_address)
-# set up transaction from constructor which executes when firstly
+
+# ===================== Set up transaction from constructor ===================== 
 tx = SimpleStorage.constructor().build_transaction(
     {
         "chainId": chain_id,
@@ -60,15 +63,17 @@ tx = SimpleStorage.constructor().build_transaction(
         "nonce": nonce,
     }
 )
+
 signed_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
 tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-print(f"Contract deployed to {tx_receipt.contractAddress}")
+print(f"Contract deployed to address {tx_receipt.contractAddress}")
 
-# Working with deployed Contracts
+# ===================== Interact with the deployed contract ===================== 
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
-print(f"Initial stored value at Retrieve {simple_storage.functions.get().call()}")
-new_transaction = simple_storage.functions.set(89128921).build_transaction(
+print(f"\nInitial value stored is: {simple_storage.functions.get().call()}")
+
+new_transaction = simple_storage.functions.set(12344321).build_transaction(
     {
         "chainId": chain_id,
         "gasPrice": w3.eth.gas_price,
@@ -76,11 +81,12 @@ new_transaction = simple_storage.functions.set(89128921).build_transaction(
         "nonce": nonce + 1,
     }
 )
+
 signed_new_txn = w3.eth.account.sign_transaction(
     new_transaction, private_key=private_key
 )
 tx_new_hash = w3.eth.send_raw_transaction(signed_new_txn.rawTransaction)
-print("Sending new transaction...")
+print("\nSending new transaction...\n")
 tx_new_receipt = w3.eth.wait_for_transaction_receipt(tx_new_hash)
 
-print(f"New stored value at Retrieve {simple_storage.functions.get().call()}")
+print(f"We have updated the value. New value is: {simple_storage.functions.get().call()}")
